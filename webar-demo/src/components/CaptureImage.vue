3f40d9209b16ce0233aa0b905c34492c42d849b0
chenhui.mch <chenhui.mch@alibaba-inc.com>
2018-09-07 12:15:11 +0800

    add face Detector

--- webar-demo/src/components/CaptureImage.vue
+++ webar-demo/src/components/CaptureImage.vue
@@ -2,6 +2,9 @@
     <div>
         <canvas class="preview-canvas" ref="canvasEl"></canvas>
         <img src="" class="pre-image" ref="preImage"/>
+        <div class="face-rect" ref="faceRect">
+
+        </div>
         <div class="control-buttons">
             <md-button class="md-raised md-primary" v-on:click="captureImage">拍照</md-button>
             <md-button class="md-raised md-primary" v-on:click="resumeCamera">重拍</md-button>
@@ -31,8 +34,9 @@
     ERROR_CODE_SYSTEM_NOT_SUPPORT,
     ERROR_CODE_INVALID_WEBGL_CONTEXT,
     // ERROR_CODE_NO_SPECIFIED_DETECTOR,
-    NO_TAKE_PHOTO_FEATURE
-  } = window.WebAR;
+    NO_TAKE_PHOTO_FEATURE,
+    FaceDetector
+  } =  window.WebAR;
   import 'vue-material/dist/vue-material.min.css'
 
   Vue.use(MdButton)
@@ -40,13 +44,22 @@
   export default {
     data() {
       return {
-        msg: 'hello vue',
-        cameraFacing: CAMERA_FACING_BACK,
+        cameraFacing: CAMERA_FACING_FRONT,
         isCameraOpened: false,
         camera: null,
         displayTarget: null,
         glContext: null,
         isCaptureImage: false,
+        haveFace: false,
+        hasNewDetectorData: false,
+        frameSize: [],
+        quality: [],
+        threeJSData: {
+          faceMatrix: [[]],
+          faceRect: [[142, 157, 192, 316]],
+          markerWidth: 134,
+          markerHeight: 200
+        },
         canvasELPos: {
           left: 0,
           top: 0,
@@ -81,7 +94,7 @@
           });
           console.debug('androidCameraFormat nv21');
           this.camera = camera;
-          await camera.openAsync();
+          await camera.openAsync(60*1000);
           this.isCameraOpened = true;
           // 配置并生成 displayTarget
           this.displayTarget = camera.createDisplayTarget(glContext, {
@@ -94,6 +107,9 @@
             window.requestAnimationFrame(draw);
           };
           window.requestAnimationFrame(draw);
+          const faceDetector = await camera.setDetectorAsync(FaceDetector);
+          this.bindDetector(faceDetector);
+          console.debug(faceDetector);
         } catch (err) {
           console.error(err);
           if (err.code === CONSTRAINT_NOT_SATISFIED_ERROR.code) {
@@ -135,6 +151,130 @@
           } else {
             // 未知错误
           }
+        }
+      },
+      bindDetector(faceDetector){
+        faceDetector.addEventListener(FaceDetector.EVENT_FACE_RECOGNIZED, data => {
+          console.debug(data.faces);
+          this.haveFace = true;
+          this.hasNewDetectorData = true;
+          // this.$refs['threeJSOverEl'].style.display = 'block';
+          // this.$refs.faceRect.style.display = 'block';
+          // console.log('haveFace', data);
+          if (this.camera.resolution && this.camera.resolution[0] !== this.frameSize[0]) {
+            this.frameSize[0] = this.camera.resolution[0];
+            this.frameSize[1] = this.camera.resolution[1];
+          }
+          this.onReceiveData(data);
+          if (!this.requestAnimationFrameId) {
+            // this.drawFaceRectByTHREEJS(this.glContext, this.size.clientWidth, this.size.clientHeight);
+          }
+        })
+        faceDetector.startRecognize();
+      },
+      onReceiveData(data) {
+        this.threeJSData.faceMatrix = data.faceMatrix;
+        this.threeJSData.faceRect = data.faceRect;
+        this.threeJSData.markerWidth = data.markerHeight;
+        this.threeJSData.markerHeight = data.markerWidth;
+        if (this.canvasELPos.width) {
+          this.quality[0] = this.frameSize[0];
+          this.quality[1] = this.frameSize[1];
+          if (data.faces && data.faces[0]) {
+            const faceRect = data.faces[0].rect;
+
+            const result = this.checkFaceRotate(faceRect, data.faces[0].matrix, data.markerWidth);
+            if (result) {
+              console.log('姿态正确');
+              this.$refs.faceRect.style.display = 'block';
+              this.drawFaceRect(this.glContext, faceRect, this.canvasELPos.width, this.canvasELPos.height);
+            } else {
+              this.$refs.faceRect.style.display = 'none';
+              console.log('姿态不正确');
+            }
+          } else {
+            this.$refs.faceRect.style.display = 'none';
+            console.log('数据错误, 找不到faces');
+          }
+          this.hasNewDetectorData = false;
+        }
+      },
+      // 检测 face 的 旋转角度
+      checkFaceRotate(faceRect, faceMatrix, markerWidth) {
+        const matrix = new THREE.Matrix4();
+        const matrix4 = new THREE.Matrix4();
+        if (faceMatrix instanceof ArrayBuffer) {
+          faceMatrix = new Float32Array(faceMatrix);
+        }
+        if (this.cameraFacing === CAMERA_FACING_FRONT && this.hasNewDetectorData) {
+          matrix4.set.apply(matrix4, faceMatrix);
+          const percent = faceRect[2] / markerWidth;
+          matrix4.transpose();
+          matrix4.scale(new THREE.Vector3(percent, percent, percent));
+
+          matrix4.multiply(new THREE.Matrix4().makeRotationX(180 * Math.PI / 180));
+          matrix4.transpose();
+          // matrix4.multiply(new THREE.Matrix4().makeRotationY(180 * Math.PI / 180));
+          const matrix4Ele = matrix4.elements;
+          if (matrix4Ele) {
+            // faceMatrix[0][11] = 300 - faceMatrix[0][11] / scaleNumX;
+            // matrix4Ele[3] = -matrix4Ele[3] / scaleNumX;
+            // matrix4Ele[7] = -matrix4Ele[7] / scaleNumX;
+          }
+          // console.log(matrix4Ele[11], matrix4Ele[7]);
+          matrix.set.apply(matrix, matrix4.elements);
+          this.pacePosMatrix = matrix;
+        } else if (this.cameraFacing === CAMERA_FACING_BACK && this.hasNewDetectorData) {
+          matrix4.set.apply(matrix4, faceMatrix);
+          const percent = faceRect[2] / markerWidth;
+          matrix4.transpose();
+          matrix4.scale(new THREE.Vector3(percent, percent, percent));
+          matrix4.transpose();
+          // OpenCV 坐标系 到 OpenGL 坐标系
+          matrix4.multiply(new THREE.Matrix4().makeRotationX(180 * Math.PI / 180));
+          // 镜像翻转
+          matrix4.multiply(new THREE.Matrix4().makeScale(-1, 1, 1));
+          const matrix4Ele = matrix4.elements;
+          if (matrix4Ele) {
+            // 增加预先估计的景深
+            matrix4Ele[11] += 300;
+          }
+          // console.log(matrix4Ele[11], matrix4Ele[7]);
+          matrix.set.apply(matrix, matrix4.elements);
+          this.pacePosMatrix = matrix;
+        }
+        var line = new THREE.Line3(
+          new THREE.Vector3(0, 0, 0),
+          new THREE.Vector3(0, 0, -1)
+        );
+        line.applyMatrix4(matrix);
+        const zAngle = line.end.angleTo(new THREE.Vector3(0, 0, -1)) * 180 / Math.PI;
+        const xAngle = line.end.angleTo(new THREE.Vector3(1, 0, 0)) * 180 / Math.PI;
+        const yAngle = line.end.angleTo(new THREE.Vector3(0, 1, 0)) * 180 / Math.PI;
+        if (Math.abs(xAngle - 90) < 15 && Math.abs(yAngle - 90) < 15 && Math.abs(zAngle) < 15) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      async drawFaceRect (webgl, faceRect, width, height) {
+        const el = this.$refs.faceRect;
+        const scaleNumX = width / this.quality[0];
+        const scaleNumY = height / this.quality[1];
+        const position = {
+          x: faceRect[0],
+          y: faceRect[1]
+        };
+        el.style.width = faceRect[2] * scaleNumX + 'px';
+        el.style.height = faceRect[3] * scaleNumY + 'px';
+        // console.log(position.x, position.y, scaleNumX, scaleNumY);
+        // 前置 后置摄像头，参数使用不一样
+        if (this.cameraFacing === CAMERA_FACING_FRONT) {
+          el.style.left = (width - faceRect[2] * scaleNumX - position.x * scaleNumX + this.canvasELPos.left) + 'px';
+          el.style.top = position.y * scaleNumY + 'px';
+        } else {
+          el.style.left = (position.x * scaleNumX + this.canvasELPos.left) + 'px';
+          el.style.top = position.y * scaleNumY + 'px';
         }
       },
       coverCanvas(event, bodyWidth, bodyHeight) {
@@ -264,5 +404,16 @@
         bottom: 0px;
         z-index: 3;
         background-color: #fff;
+    }
+
+    .face-rect {
+        position: fixed;
+        z-index: 4;
+        top: 0;
+        left: 0;
+        width: 100px;
+        height: 100px;
+        border: 1px solid red;
+        display: none;
     }
 </style>
